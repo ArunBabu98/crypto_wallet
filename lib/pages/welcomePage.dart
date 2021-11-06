fe22336e58d4b3950b424444ddf73b863ba2b505
xena <1runx3na2@gmail.com>
2021-11-06 13:09:15 +0530

    Home Page UI

--- lib/pages/welcomePage.dart
+++ lib/pages/welcomePage.dart
@@ -18,9 +18,62 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        
-      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.amberAccent,
+            height: 150,
+            alignment: Alignment.center,
+            child: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                image: DecorationImage(image: AssetImage('assets/images/logo.png')))
+              ),
+            ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: const Text(
+              "User Name",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold
+              ),
+              ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            alignment: Alignment.center,
+            height: 100,
+            width: MediaQuery.of(context).size.width,
+            // color: Colors.black,
+            child: const Text(
+              "Balance",
+              style: TextStyle(
+                fontSize: 70,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold
+              ),
+              ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(5),
+            alignment: Alignment.center,
+            height: 50,
+            width: MediaQuery.of(context).size.width,
+            // color: Colors.black,
+            child: const Text(
+              "300\$",
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold
+              ),
+              ),
+          ),
+        ],
+      )
     );
   }
 }
