2520ded1c2ca19286c09c8bc5ec1185b7d11ada5
xena <1runx3na2@gmail.com>
2021-11-12 15:03:42 +0530

    error change

--- lib/utilities/sign_in_page.dart
+++ lib/utilities/sign_in_page.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
